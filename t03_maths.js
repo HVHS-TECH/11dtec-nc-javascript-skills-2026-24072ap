f00443ee5be38c5b8f147472b07f482b3136634f
24072ap <24072ap@hvhs.school.nz>
2026-04-29 02:59:01 +0000

    Apr 29, 2026, 2:59 AM

--- t03_maths.js
+++ t03_maths.js
@@ -11,6 +11,7 @@ let birthyear;
 let oldage;
 oldAge = age + 10;
 birthyear = year - age;
+halfMoney = money / 2; 
 
 
 /********************************
@@ -20,6 +21,9 @@ birthyear = year - age;
 console.log("hi "+ Username + " as of " +  + " years old. You have " + money + "dollars ")
 console.log("You were born in " + birthyear)
 console.log("In 10 years you will be " + oldAge + "years old")
+console.log("You have " + money + " dollars")
+console.log( "You spent half of your money, now you have " + halfMoney)
+console.log("Then you get $3, now you have" + halfMoney + 3)
 /********************************
  Functions
  ********************************/ 
