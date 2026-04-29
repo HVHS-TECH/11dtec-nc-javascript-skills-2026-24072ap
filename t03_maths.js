310c0c8d04f86bd542391ecf3d16f55d7250bc29
24072ap <24072ap@hvhs.school.nz>
2026-04-29 02:44:53 +0000

    Apr 29, 2026, 2:44 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,19 @@
-/* Paste your code from task 2 here */
+/****************************************
+ * maths
+ */
+
+//variables
+let Username = "Anish";
+let year = 2026;
+let age = 15;
+let money = 20;
+
+/********************************
+ Main code
+ ********************************/
+
+console.log("hi "+ Username + " as of " +  + " years old. You have " + money + "dollars ")
+
+/********************************
+ Functions
+ ********************************/ 
