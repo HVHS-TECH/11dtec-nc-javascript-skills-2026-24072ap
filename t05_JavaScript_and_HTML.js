9ae287803c43b1ae2f91991e2abbcc300a7d70ac
24072ap <24072ap@hvhs.school.nz>
2026-05-03 23:05:43 +0000

    May 3, 2026, 11:05 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -25,20 +25,20 @@ halfMoney = money / 2;
  const OUTPUT = document.getElementById("JavaScriptOutput");
  OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
 
-OUTPUT.innerHTML += ("Hi " + username + <br></br>)
-OUTPUT.innerHTML += ("As of " + year + " you are " + age + " years old" + <br></br>)
-OUTPUT.innerHTML += ("You were born in " + (year - age) + <br></br>)
-OUTPUT.innerHTML += ("In 10 years you will be " + newAge + " years old" + <br></br>)
-OUTPUT.innerHTML += ("You have " + pocketMoney + " dollars" + <br></br>)
-OUTPUT.innerHTML += ("You spend half of your money, now you have " + halfMoney + <br></br>)
-OUTPUT.innerHTML += ("Then you get $3, now you have " + (halfMoney + 3) + <br></br>)
+OUTPUT.innerHTML += ("Hi " + username + <br></br>);
+OUTPUT.innerHTML += ("As of " + year + " you are " + age + " years old" + <br></br>);
+OUTPUT.innerHTML += ("You were born in " + (year - age) + <br></br>);
+OUTPUT.innerHTML += ("In 10 years you will be " + newAge + " years old" + <br></br>);
+OUTPUT.innerHTML += ("You have " + pocketMoney + " dollars" + <br></br>);
+OUTPUT.innerHTML += ("You spend half of your money, now you have " + halfMoney + <br></br>);
+OUTPUT.innerHTML += ("Then you get $3, now you have " + (halfMoney + 3) + <br></br>);
 
-console.log("hi "+ Username + " as of " +  + " years old. You have " + money + "dollars ")
-console.log("You were born in " + birthyear)
-console.log("In 10 years you will be " + oldAge + " years old")
-console.log("You have " + money + " dollars")
-console.log( "You spent half of your money, now you have " + halfMoney)
-console.log("Then you get $3, now you have" + halfMoney + 3 + " dollars")
+console.log("hi "+ Username + " as of " +  + " years old. You have " + money + "dollars ");
+console.log("You were born in " + birthyear);
+console.log("In 10 years you will be " + oldAge + " years old");
+console.log("You have " + money + " dollars");
+console.log( "You spent half of your money, now you have " + halfMoney);
+console.log("Then you get $3, now you have" + halfMoney + 3 + " dollars");
 /********************************
  Functions
  ********************************/ 
