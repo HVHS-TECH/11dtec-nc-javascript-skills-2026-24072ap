1dd4563ba098462a99b2c2ca560ed7aa10c62af2
24072ap <24072ap@hvhs.school.nz>
2026-05-03 22:38:29 +0000

    May 3, 2026, 10:38 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,30 @@
 /* Paste your code from task 3, Maths here */
+/****************************************
+ * maths
+ */
+
+//variables
+let Username = "Anish";
+let year = 2026;
+let age = 15;
+let money = 20;
+let birthyear;
+let oldage;
+oldAge = age + 10;
+birthyear = year - age;
+halfMoney = money / 2; 
+
+
+/********************************
+ Main code
+ ********************************/
+
+console.log("hi "+ Username + " as of " +  + " years old. You have " + money + "dollars ")
+console.log("You were born in " + birthyear)
+console.log("In 10 years you will be " + oldAge + " years old")
+console.log("You have " + money + " dollars")
+console.log( "You spent half of your money, now you have " + halfMoney)
+console.log("Then you get $3, now you have" + halfMoney + 3 + " dollars")
+/********************************
+ Functions
+ ********************************/ 
