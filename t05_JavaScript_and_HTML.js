aa1ec7226c56dcd90734994b3383661ba7ad7452
24072ap <24072ap@hvhs.school.nz>
2026-05-04 23:59:18 +0000

    May 4, 2026, 11:59 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -25,13 +25,13 @@ halfMoney = money / 2;
  const OUTPUT = document.getElementById("JavaScriptOutput");
  OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
 
-OUTPUT.innerHTML += ("<p>""Hi " + username + "</p>");
-OUTPUT.innerHTML += ("<p>""As of " + year + " you are " + age + " years old" + "</p>");
-OUTPUT.innerHTML += ("<p>""You were born in " + (year - age) + "</p>");
-OUTPUT.innerHTML += ("<p>""In 10 years you will be " + newAge + " years old" + "</p>");
-OUTPUT.innerHTML += ("<p>""You have " + pocketMoney + " dollars" + "</p>" );
-OUTPUT.innerHTML += ("<p>""You spend half of your money, now you have " + halfMoney + "</p>" );
-OUTPUT.innerHTML += ("<p>""Then you get $3, now you have " + (halfMoney + 3) + "</p>" );
+OUTPUT.innerHTML += ("<p>Hi " + Username + "</p>");
+OUTPUT.innerHTML += ("<p>As of " + year + " you are " + age + " years old" + "</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + (year - age) + "</p>");
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + newAge + " years old" + "</p>");
+OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars" + "</p>" );
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + "</p>" );
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + "</p>" );
 
 
 /********************************
