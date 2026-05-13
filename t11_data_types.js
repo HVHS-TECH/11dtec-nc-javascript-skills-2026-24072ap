5307c19e20775c8f7946163636971c0a3bed140a
24072ap <24072ap@hvhs.school.nz>
2026-05-13 02:19:37 +0000

    May 13, 2026, 2:19 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,71 @@
-/* Paste your code from the last task */
+/* Paste your code from task 7 here*/
+
+/****************************************
+ * maths
+ */
+
+console.log("Running t05_JavaScript_and_HTML.js");
+
+
+//variables
+let Username = "Anish";
+let year = 2026;
+let age = 15;
+let money = 20;
+let birthyear;
+let oldage;
+let pocketMoney = 20;
+let newAge = age + 10;
+birthyear = year - age;
+halfMoney = money / 2; 
+
+
+/********************************
+ Main code
+ ********************************/
+
+ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+ OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
+
+ function start(){
+OUTPUT.innerHTML += ("<p>Hi " + Username + "</p>");
+OUTPUT.innerHTML += ("<p>As of " + year + " you are " + age + " years old" + "</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + (year - age) + "</p>");
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + newAge + " years old" + "</p>");
+OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars" + "</p>" );
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + "</p>" );
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + "</p>" );
+displayProduct("Chocolate bar", "4")
+displayProduct("Chips", "3")
+displayProduct("Drink", "2.50")
+OUTPUT.innerHTML += "<p>Less code matters</p>"
+ }
+
+
+/********************************
+ Functions
+ ********************************/ 
+
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
+}
+
+/*
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+} 
+    */
+
+function getFormInput(){
+    let userAge = Number(document.getElementById("ageField").value);
+        let userMoney = Number(document.getElementById("moneyField").value);
+
+    age = userAge;
+    pocketMoney = userMoney;
+    halfMoney = pocketMoney / 2;
+    newAge = age + 10;
+
+    start();
+}
