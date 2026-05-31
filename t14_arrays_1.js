acebd8aef0d70ec3d0c5f28b791a2f2fabbbef88
24072ap <24072ap@hvhs.school.nz>
2026-05-31 09:30:32 +0000

    May 31, 2026, 9:30 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -49,7 +49,7 @@ OUTPUT.innerHTML += "<p>Less code matters</p>"
   OUTPUT.innerHTML = classArray;
 
 
-OUTPUT.innerHTML = "The fifth person in the class is "+ classArray[choice];
+OUTPUT.innerHTML = "The " + choice + " person in the class is "+ classArray[choice];
 
  }
 
