ff7b2b083164726348e1d9472314dce8d4e478de
24072ap <24072ap@hvhs.school.nz>
2026-06-01 05:18:35 +0000

    Jun 1, 2026, 5:18 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -15,18 +15,18 @@ console.log("Running t05_JavaScript_and_HTML.js");
  OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
 
 function start(){
-const SHOPPING_FORM = document.getElementById("welcomeForm");
-if (SHOPPING_FORM.checkValidity()=== false){
-    OUTPUT.innerHTML += "Please fill out all fields correctly. <br>";
-} else {
-    let userName = document.getElementById("input_name").value;
-    OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
-}
-if (AGE_FIELD.value > 13 && AGE_FIELD.value < 99) {
-    OUTPUT.innerHTML = "You're a good age<br>";
-}
+    const SHOPPING_FORM = document.getElementById("welcomeForm");
+    if (AGE_FIELD.value > 13 && AGE_FIELD.value < 99) {
+        OUTPUT.innerHTML = "You're a good age<br>";
+    }
 
-if (AGE_FIELD.value < 13 || AGE_FIELD.value > 99){
-    OUTPUT.innerHTML = "You're not a good age<br>";
-}
+    if (AGE_FIELD.value < 13 || AGE_FIELD.value > 99){
+        OUTPUT.innerHTML = "You're not a good age<br>";
+    }
+    if (SHOPPING_FORM.checkValidity()=== false){
+        OUTPUT.innerHTML += "Please fill out all fields correctly. <br>";
+    } else {
+        let userName = document.getElementById("input_name").value;
+        OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
+    }
 }
