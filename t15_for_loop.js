4d31882e2d10b8c213a072133165841c3a938e5a
24072ap <24072ap@hvhs.school.nz>
2026-06-01 05:15:25 +0000

    Jun 1, 2026, 5:15 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -11,6 +11,9 @@ console.log("Running t05_JavaScript_and_HTML.js");
  Main code
  ********************************/
 
+ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+ OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
+
 function start(){
 const SHOPPING_FORM = document.getElementById("welcomeForm");
 if (AGE_FIELD.value > 13 && AGE_FIELD.value < 99) {
@@ -20,12 +23,12 @@ if (AGE_FIELD.value > 13 && AGE_FIELD.value < 99) {
 if (AGE_FIELD.value < 13 || AGE_FIELD.value > 99){
     OUTPUT.innerHTML = "You're not a good age<br>";
 }
-/*if (SHOPPING_FORM.checkValidity()=== false){
+if (SHOPPING_FORM.checkValidity()=== false){
     OUTPUT.innerHTML += "Please fill out all fields correctly. <br>";
 } else {
     let userName = document.getElementById("input_name").value;
     OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
-}*/
+}
 
 
 }
