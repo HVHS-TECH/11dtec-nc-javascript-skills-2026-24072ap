6aae74bf8ea84b17586d9109b0d37292de87bbd5
24072ap <24072ap@hvhs.school.nz>
2026-06-02 08:15:11 +0000

    Jun 2, 2026, 8:15 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -7,6 +7,19 @@
 console.log("Running t05_JavaScript_and_HTML.js");
 
 
+//variables
+let Username = "Anish";
+let year = 2026;
+let age = 15;
+let money = 20;
+let birthyear;
+let oldage;
+let pocketMoney = 20;
+let newAge = age + 10;
+birthyear = year - age;
+halfMoney = money / 2; 
+
+
 /********************************
  Main code
  ********************************/
@@ -14,19 +27,73 @@ console.log("Running t05_JavaScript_and_HTML.js");
  const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
  OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
 
-function start(){
-    const SHOPPING_FORM = document.getElementById("welcomeForm");
-    if (AGE_FIELD.value > 13 && AGE_FIELD.value < 99) {
-        OUTPUT.innerHTML = "You're a good age<br>";
+ function start(){
+OUTPUT.innerHTML += ("<p>Hi " + Username + "</p>");
+OUTPUT.innerHTML += ("<p>As of " + year + " you are " + age + " years old" + "</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + (year - age) + "</p>");
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + newAge + " years old" + "</p>");
+OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars" + "</p>" );
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + "</p>" );
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + "</p>" );
+displayProduct("Chocolate bar", "4")
+displayProduct("Chips", "3")
+displayProduct("Drink", "2.50")
+OUTPUT.innerHTML += "<p>Less code matters</p>"
+  OUTPUT.innerHTML += "This week's pay:<br>";
+  OUTPUT.innerHTML += "Monday: $"+mondayPay+"<br>";
+  OUTPUT.innerHTML += "Tuesday: $"+tuesdayPay+"<br>";
+ }
+
+
+
+
+/********************************
+ Functions
+ ********************************/ 
+
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
+}
+
+/*
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+} 
+    */
+
+function getFormInput(){
+    let userAge = Number(document.getElementById("ageField").value);
+        let userMoney = Number(document.getElementById("moneyField").value);
+
+    age = userAge;
+    pocketMoney = userMoney;
+    halfMoney = pocketMoney / 2;
+    newAge = age + 10;
+
+    start();
+    
+    if (pocketMoney >= 4){
+        OUTPUT.innerHTML += "<p>You can afford a chocolate bar millionare brodie</p>"
     }
 
-    if (AGE_FIELD.value < 13 || AGE_FIELD.value > 99){
-        OUTPUT.innerHTML = "You're not a good age<br>";
+    if (pocketMoney <= 3.99){
+        OUTPUT.innerHTML += "<p>You cannot afford a chocolate bar homeless kid</p>"
     }
-    if (SHOPPING_FORM.checkValidity()=== false){
-        OUTPUT.innerHTML += "Please fill out all fields correctly. <br>";
-    } else {
-        let userName = document.getElementById("input_name").value;
-        OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
-    }
+
+}
+
+ function calculatePay(_hours){
+    return _hours * 10;
+  }
+
+  let mondayPay = calculatePay(8);
+  let tuesdayPay = calculatePay(6);
+ 
+
+ 
+
+for(let i=99; i>0; i--){
+    console.log( + i + " bottles of milk on the wall, " + i + " bottles of milk. Take one down and pass it around, " + (i-1) + " bottles of milk on the wall.")
 }
--- t18_validation.js
+++ t18_validation.js
@@ -1,1 +1,32 @@
-/* Paste your code from the last task */
+/* Paste your code from task 7 here*/
+
+/****************************************
+ * maths
+ */
+
+console.log("Running t05_JavaScript_and_HTML.js");
+
+
+/********************************
+ Main code
+ ********************************/
+
+ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+ OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
+
+function start(){
+    const SHOPPING_FORM = document.getElementById("welcomeForm");
+    if (AGE_FIELD.value > 13 && AGE_FIELD.value < 99) {
+        OUTPUT.innerHTML = "You're a good age<br>";
+    }
+
+    if (AGE_FIELD.value < 13 || AGE_FIELD.value > 99){
+        OUTPUT.innerHTML = "You're not a good age<br>";
+    }
+    if (SHOPPING_FORM.checkValidity()=== false){
+        OUTPUT.innerHTML += "Please fill out all fields correctly. <br>";
+    } else {
+        let userName = document.getElementById("input_name").value;
+        OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
+    }
+}
