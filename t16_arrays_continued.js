48ecbcf29d6b6558555a220ff84a76ab04fbbaf1
24072ap <24072ap@hvhs.school.nz>
2026-06-03 02:43:27 +0000

    Jun 3, 2026, 2:43 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -8,12 +8,13 @@
  
  let classArray = ["Alice", "Bob", "Kaia", "Olivia", "Aroha", "Josh"];
 
-  let ordinals = ["first", "second", "third", "fourth", "fifth", "sixth"];
-  let choice = CHOOSE_FIELD.value;
-  let studentNumber = ordinals[choice];
-  OUTPUT.innerHTML = "You chose: "+classArray[choice]+ "<br>";
-  OUTPUT.innerHTML = classArray;
-  OUTPUT.innerHTML = "There are "+ classArray.length +"people in the array.";
+  OUTPUT.innerHTML = "There are "+ classArray.length +" people in the array.";
+
+  for(let i=0; i<classArray.length; i++){
+
+    OUTPUT.innerHTML += "Person "+ i +": "+classArray[i]+"<br>";
+    
+  }
 
 
 OUTPUT.innerHTML = "The " + studentNumber + " person in the class is "+ classArray[choice];
