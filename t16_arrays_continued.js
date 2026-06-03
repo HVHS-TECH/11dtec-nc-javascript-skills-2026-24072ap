e38fbc57d5a1d5db4190897a00b396000567fe44
24072ap <24072ap@hvhs.school.nz>
2026-06-03 02:31:02 +0000

    Jun 3, 2026, 2:31 AM

---
+++ t16_arrays_continued.js
@@ -0,0 +1,19 @@
+ 
+ 
+ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+ OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
+
+ function start(){
+ 
+ 
+ let classArray = ["Alice", "Bob", "Kaia", "Olivia", "Aroha", "Josh"];
+  let ordinals = ["first", "second", "third", "fourth", "fifth", "sixth"];
+  let choice = CHOOSE_FIELD.value;
+  let studentNumber = ordinals[choice];
+  OUTPUT.innerHTML = "You chose: "+classArray[choice]+ "<br>";
+  OUTPUT.innerHTML = classArray;
+
+
+OUTPUT.innerHTML = "The " + studentNumber + " person in the class is "+ classArray[choice];
+
+ }
