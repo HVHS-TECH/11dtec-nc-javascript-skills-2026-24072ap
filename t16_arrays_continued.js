a96b07147af1b7c7ba5d57f39b44dd72c68c39dc
24072ap <24072ap@hvhs.school.nz>
2026-06-04 21:20:07 +0000

    Jun 4, 2026, 9:20 PM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -6,21 +6,6 @@
  function start(){
  
  
- let classArray = ["Alice", "Bob", "Kaia", "Olivia", "Aroha", "Josh"];
- classArray.splice(3,1);
-
-  OUTPUT.innerHTML = "There are "+ classArray.length +" people in the array.";
-
-  for(let i=0; i<classArray.length; i++){
-
-    OUTPUT.innerHTML += "Person "+ i +": "+classArray[i]+"<br>";
-
-  }
-
-
-OUTPUT.innerHTML = "The " + studentNumber + " person in the class is "+ classArray[choice];
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Shopping list</h2>";
 
 let shoppingList = [];
 
